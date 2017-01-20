2c88e41b9e659a26ccf4e7a2bebb34712cd20ab7
Baptiste Bohelay <baptistebohelay@Baptistes-MacBook-Pro.local>
2017-01-20 16:15:34 +0200

    Work on the flower with scale, to be continues later

--- pages/scales/js/Particle.js
+++ pages/scales/js/Particle.js
@@ -2,19 +2,25 @@
  * Created by baptistebohelay on 17/01/17.
  */
 "use strict";
-function Particle(standardMaterial)
+function Particle(standardMaterial, aGeometry)
 {
-    var geometry = new THREE.PlaneGeometry( 10, 10, 15, 15 );
+    var geometry = aGeometry;
     this.mesh = new THREE.Mesh( geometry, standardMaterial );
     //this.mesh.scale.set(0.1, 0.1, 0.1);
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
     //this.mesh.position.set(Math.random() * 100, Math.random() * 100, Math.random() * 100);
     this.mesh.rotation.set(Math.random() * 100, Math.random() * 100, Math.random() * 100);
+    this.time = 0;
+    this.positionInit = null;
+    this.rotationInit = null;
+    this.normHeight = 0;
 }
 
-Particle.prototype.setPosition = function(aPosition){
-  this.mesh.position.set(aPosition.x, aPosition.y, aPosition.z);
+Particle.prototype.setPosition = function(aPosition, normHeight){
+    this.positionInit = aPosition.clone();
+    this.normHeight = normHeight;
+    this.mesh.position.set(aPosition.x, aPosition.y, aPosition.z);
 };
 
 Particle.prototype.setScale = function(aScale){
@@ -29,15 +35,21 @@ Particle.prototype.lookAt = function(aPosition, rotateX){
     this.mesh.lookAt(aPosition);
     this.mesh.rotateY(Math.PI);
     this.mesh.rotateX(rotateX);
+    this.rotationInit = this.mesh.rotation.clone();
 };
-
 
 Particle.prototype.init = function(aOptions) {
 
 };
 
-Particle.updateGlobal = function(){}
 
 Particle.prototype.update = function(aDelta) {
-
+    this.time += aDelta;
+    if(this.mesh.position.y > -10) {
+        this.mesh.rotation.set(this.rotationInit.x, this.rotationInit.y, this.rotationInit.z);
+        var lAngle = (0.5 + Math.cos(-this.time + this.mesh.position.y * 0.5));
+        this.mesh.rotateX(this.mesh.position.y * 0.07 * lAngle);
+        var lRadiusChange = 1;
+        this.mesh.position.set(this.positionInit.x * lRadiusChange, this.positionInit.y, this.positionInit.z * lRadiusChange);
+    }
 };
--- pages/scales/js/ParticleManager.js
+++ pages/scales/js/ParticleManager.js
@@ -9,8 +9,8 @@ function ParticleManager(aScene)
     standardMaterial = new THREE.MeshStandardMaterial( {
         bumpScale: - 0.05,
         color: 0xffffff,
-        metalness: 0.9,
-        roughness: 0.2,
+        metalness: 0.2,
+        roughness: 0,
         shading: THREE.SmoothShading,
         premultipliedAlpha: true,
         transparent: true
@@ -21,15 +21,35 @@ function ParticleManager(aScene)
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
-        map.repeat.set( 9, 0.5 );
+        //map.repeat.set( 9, 0.5 );
         standardMaterial.map = map;
         standardMaterial.needsUpdate = true;
     } );
+
+    textureLoader.load( "textures/brick_alpha.jpg", function( map ) {
+        //map.wrapS = THREE.RepeatWrapping;
+        //map.wrapT = THREE.RepeatWrapping;
+        map.anisotropy = 4;
+        //map.repeat.set( 9, 0.5 );
+        standardMaterial.alphaMap = map;
+        standardMaterial.needsUpdate = true;
+    } );
+
+    textureLoader.load( "textures/brick_displace.jpg", function( map ) {
+        map.wrapS = THREE.RepeatWrapping;
+        map.wrapT = THREE.RepeatWrapping;
+        map.anisotropy = 4;
+        //map.repeat.set( 9, 0.5 );
+        standardMaterial.displacementMap = map;
+        standardMaterial.displacementScale = -2;
+        standardMaterial.needsUpdate = true;
+    } );
+
     textureLoader.load( "textures/brick_bump.jpg", function( map ) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
-        map.repeat.set( 9, 0.5 );
+        //map.repeat.set( 9, 0.5 );
         standardMaterial.bumpMap = map;
         standardMaterial.needsUpdate = true;
     } );
@@ -37,40 +57,65 @@ function ParticleManager(aScene)
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
-        map.repeat.set( 9, 0.5 );
+        //map.repeat.set( 9, 0.5 );
         standardMaterial.roughnessMap = map;
         standardMaterial.needsUpdate = true;
     } );
 
+    standardMaterial.side = THREE.DoubleSide;
+    var geometry = new THREE.PlaneGeometry( 10, 10, 4, 4 );
+
     this.group = new THREE.Group();
-    var radius = 20;
-    for(var azimut = 0; azimut < Math.PI * 2; azimut += 0.3)
+    var radius = 13;
+    for(var height = 0; height < 1; height+= 0.03)
     {
-        for(var height = 0; height < 1; height+= 0.05)
+        var radiusCoeff = Math.sin(height * Math.PI);
+        var numAzimut = radiusCoeff * 40 * (1 + height * 2.3);
+        for(var i = 0; i < numAzimut; i++)
         {
-            var particle = new Particle(standardMaterial);
+            var azimut = 2 * Math.PI * i / numAzimut;
+            var particle = new Particle(standardMaterial, geometry);
 
             this.particles.push(particle);
             this.group.add( particle.mesh );
 
-            var x = Math.cos(azimut) * radius * height;
-            var z = Math.sin(azimut) * radius * height;
-            var y = -radius * 0.5 + height * radius * 2;
-            var rotationX = 0;
-            var rotationY = azimut;
+            var x = Math.cos(azimut) * radius * radiusCoeff;
+            var z = Math.sin(azimut) * radius * radiusCoeff;
+            var y = -radius * 1 + height * radius * 2;
+            var rotationX = 0.6;
+            //var rotationY = azimut;
             var rotationZ = 0;
-            particle.setPosition(new THREE.Vector3(x, y, z));
+            particle.setPosition(new THREE.Vector3(x, y, z), height);
             //particle.setRotation(new THREE.Vector3(rotationX,rotationY, rotationZ));
-            particle.lookAt(new THREE.Vector3(0,0, 0), height);
-            particle.setScale(0.2);
+            particle.lookAt(new THREE.Vector3(0,0, 0), height * 0.1);
+            particle.setScale(0.3);
         }
     }
 
     aScene.add(this.group );
+
+    var geometryBigSphere = new THREE.SphereGeometry( radius, radius, 15, 15 );
+    var sphereMaterial = new THREE.MeshStandardMaterial( {
+        color: 0xffffff,
+        emissive: 0xffffff,
+        metalness: 0.9,
+        roughness: 0.2,
+        shading: THREE.SmoothShading,
+        premultipliedAlpha: true,
+        transparent: true
+    } );
+    var bigSphereMesh = new THREE.Mesh( geometryBigSphere, sphereMaterial );
+    var lScale = 0.5;
+    bigSphereMesh.scale.set(lScale, lScale, lScale);
+    bigSphereMesh.position.set(0,-15,0);
+    //bigSphereMesh.position.set(-20,-30,-20);
+    bigSphereMesh.castShadow = true;
+    bigSphereMesh.receiveShadow = true;
+    aScene.add(bigSphereMesh);
 }
 
 ParticleManager.prototype.update = function(aDelta) {
-    this.group.rotation.y += 0.01;
+    this.group.rotation.y += 0.001;
     for(var i = 0; i < this.particles.length; i++)
     {
         this.particles[i].update(aDelta);
