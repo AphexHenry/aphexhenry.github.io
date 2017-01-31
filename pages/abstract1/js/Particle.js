d69198da6975f855708a5c8eda4a148eadfc33d4
Baptiste Bohelay <baptistebohelay@Baptistes-MacBook-Pro.local>
2017-02-01 01:44:13 +0200

    abstract1 : Make multiple snakes

--- pages/abstract1/js/Particle.js
+++ pages/abstract1/js/Particle.js
@@ -2,33 +2,74 @@
  * Created by baptistebohelay on 17/01/17.
  */
 "use strict";
-function Particle(standardMaterial, aGeometry)
+function Particle()
 {
-    var geometry = aGeometry;
-    this.mesh = new THREE.Mesh( geometry, standardMaterial );
-    //this.mesh.scale.set(0.1, 0.1, 0.1);
+    var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+    var vertexShader2 = document.getElementById( 'vertexShader2' ).textContent;
+    var fragmentShader = THREE.ShaderChunk[ 'meshphong_frag' ];//document.getElementById( 'fragmentShader' ).textContent;
+
+    var uniforms = THREE.UniformsUtils.merge( [
+
+        THREE.ShaderLib[ 'phong' ].uniforms,
+        {
+            time: { type: "f", value: 1.0 },
+            random: {type: "f", value: 1.0}
+        }
+
+    ] );
+
+    this.material = new THREE.ShaderMaterial( {
+        uniforms:uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    } );
+
+    var materialColor = 0x030303;
+
+    // Material attributes from MeshPhongMaterial
+    this.material.color = new THREE.Color( materialColor );
+    this.material.specular = new THREE.Color( 0x222222 );
+    this.material.shininess = 50;
+
+    // Sets the uniforms with the material values
+    this.material.uniforms.diffuse.value = new THREE.Color( 0x020202 );
+    this.material.uniforms.specular.value = this.material.specular;
+    this.material.uniforms.shininess.value = 10;//Math.max( this.material.shininess, 1e-4 );
+    this.material.uniforms.opacity.value =this. material.opacity;
+
+    this.randomShader = Math.random();
+    this.material.uniforms.random.value = this.randomShader;
+    this.material.lights = true;
+
+    //this.material = new THREE.ShaderMaterial( {
+    //    //uniforms:uniforms,
+    //    vertexShader: vertexShader2,
+    //    fragmentShader: fragmentShader
+    //} );
+
+    // create a sphere and assign the material
+    this.mesh = new THREE.Mesh(
+        new THREE.IcosahedronGeometry( 5, 4 ),
+        this.material
+    );
+
+    // magic here
+    this.mesh.customDepthMaterial = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms: uniforms
+    });
+
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
-    //this.mesh.position.set(Math.random() * 100, Math.random() * 100, Math.random() * 100);
-    this.mesh.rotation.set(Math.random() * 100, Math.random() * 100, Math.random() * 100);
+
     this.time = 0;
-    this.positionInit = null;
-    this.rotationInit = null;
-    this.normHeight = 0;
+    this.mesh.position.z = 70 + this.randomShader * -230;
+
 }
 
-Particle.prototype.setPosition = function(aPosition, normHeight){
-    this.positionInit = aPosition.clone();
-    this.normHeight = normHeight;
-    this.mesh.position.set(aPosition.x, aPosition.y, aPosition.z);
-};
-
-Particle.prototype.setScale = function(aScale){
-  this.mesh.scale.set(aScale, aScale, aScale);
-};
-
-Particle.prototype.setRotation = function(aPosition){
-    this.mesh.rotation.set(aPosition.x, aPosition.y, aPosition.z);
+Particle.prototype.getMesh = function(){
+    return this.mesh;
 };
 
 Particle.prototype.lookAt = function(aPosition, rotateX){
@@ -44,11 +85,7 @@ Particle.prototype.init = function(aOptions) {
 
 Particle.prototype.update = function(aDelta) {
     this.time += aDelta;
-    //if(this.mesh.position.y > -10) {
-        this.mesh.rotation.set(this.rotationInit.x, this.rotationInit.y, this.rotationInit.z);
-        var lAngle = (0.5 + Math.cos(-this.time + this.mesh.position.y * 0.5));
-        this.mesh.rotateX(this.mesh.position.y * 0.07 * lAngle);
-        var lRadiusChange = Math.cos(this.time + this.positionInit.y * 0.1);
-        this.mesh.position.set(this.positionInit.x * lRadiusChange, this.positionInit.y * lRadiusChange, this.positionInit.z * lRadiusChange);
-    //}
+    this.mesh.customDepthMaterial.uniforms.time.value = this.time;
+    //this.material.uniforms.time.value = this.time;
+    this.mesh.customDepthMaterial.needsUpdate = true;
 };
--- pages/abstract1/js/ParticleManager.js
+++ pages/abstract1/js/ParticleManager.js
@@ -4,78 +4,28 @@
 "use strict";
 function ParticleManager(aScene)
 {
-    var vertexShader = document.getElementById( 'vertexShader' ).textContent;
-    var vertexShader2 = document.getElementById( 'vertexShader2' ).textContent;
-    var fragmentShader = THREE.ShaderChunk[ 'meshphong_frag' ];//document.getElementById( 'fragmentShader' ).textContent;
-
-    var uniforms = THREE.UniformsUtils.merge( [
-
-        THREE.ShaderLib[ 'phong' ].uniforms,
-        {time: { type: "f", value: 1.0 }}
-
-    ] );
-
-    this.material = new THREE.ShaderMaterial( {
-        uniforms:uniforms,
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader
-    } );
-
-    var materialColor = 0x030303;
-
-    // Material attributes from MeshPhongMaterial
-    this.material.color = new THREE.Color( materialColor );
-    this.material.specular = new THREE.Color( 0x222222 );
-    this.material.shininess = 50;
-
-    // Sets the uniforms with the material values
-    this.material.uniforms.diffuse.value = new THREE.Color( 0x020202 );
-    this.material.uniforms.specular.value = this.material.specular;
-    this.material.uniforms.shininess.value = 10;//Math.max( this.material.shininess, 1e-4 );
-    this.material.uniforms.opacity.value =this. material.opacity;
-    this.material.lights = true;
-
-    //this.material = new THREE.ShaderMaterial( {
-    //    //uniforms:uniforms,
-    //    vertexShader: vertexShader2,
-    //    fragmentShader: fragmentShader
-    //} );
-
-    // create a sphere and assign the material
-    this.mesh = new THREE.Mesh(
-        new THREE.IcosahedronGeometry( 5, 4 ),
-        this.material
-    );
-
-    // magic here
-    this.mesh.customDepthMaterial = new THREE.ShaderMaterial({
-        vertexShader: vertexShader,
-        fragmentShader: fragmentShader,
-        uniforms: uniforms
-    });
-
-    this.mesh.castShadow = true;
-    //this.mesh.receiveShadow = true;
-
     this.group = new THREE.Group();
-    this.group.add(this.mesh);
+    this.particles = [];
+    for(var i = 0; i < 5; i++)
+    {
+        var lParticle = new Particle();
+        var mesh = lParticle.getMesh();
+        this.particles.push(lParticle);
+        this.group.add(mesh);
+    }
 
     this.time = 0;
-
     aScene.add(this.group );
-
 }
 
 ParticleManager.prototype.update = function(aDelta) {
     //this.group.rotation.y += aDelta * 0.2;
     this.group.rotation.y = Math.PI * 0.5;
-    this.group.position.x = -50//100  * Math.cos(this.time * 0.5);
+    this.group.position.x = -50 ;//100  * Math.cos(this.time * 0.5);
     this.time += aDelta;
-    this.mesh.customDepthMaterial.uniforms.time.value = this.time;
-    //this.material.uniforms.time.value = this.time;
-    this.mesh.customDepthMaterial.needsUpdate = true;
-    //for(var i = 0; i < this.particles.length; i++)
-    //{
-    //    this.particles[i].update(aDelta);
-    //}
+
+    for(var i = 0; i < this.particles.length; i++)
+    {
+        this.particles[i].update(aDelta);
+    }
 };
