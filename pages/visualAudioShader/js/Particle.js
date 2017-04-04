7084c5c0f63e7007558a38ac564c331b7680d57a
Baptiste Bohelay <baptistebohelay@Baptistes-MacBook-Pro.local>
2017-04-05 00:05:51 +0300

    Shader Audio Visual

---
+++ pages/visualAudioShader/js/Particle.js
@@ -0,0 +1,107 @@
+/**
+ * Created by baptistebohelay on 17/01/17.
+ */
+"use strict";
+function Particle()
+{
+    var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+    var vertexShader2 = document.getElementById( 'vertexShader2' ).textContent;
+    var fragmentShader = THREE.ShaderChunk[ 'meshphong_frag' ];//document.getElementById( 'fragmentShader' ).textContent;
+
+    var zeroArray = [];
+    for(var i = 0; i < 350; i++)
+    {
+        zeroArray.push(i / 350);
+    }
+
+    var uniforms = THREE.UniformsUtils.merge( [
+
+        THREE.ShaderLib[ 'phong' ].uniforms,
+        {
+            time: { type: "f", value: 1.0 },
+            random: {type: "f", value: 1.0},
+            amplitudes: {type: "fv", value: zeroArray}
+        }
+
+    ] );
+
+    this.material = new THREE.ShaderMaterial( {
+        uniforms:uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader
+    } );
+
+    var materialColor = 0x030303;
+
+    // Material attributes from MeshPhongMaterial
+    this.material.color = new THREE.Color( materialColor );
+    this.material.specular = new THREE.Color( 0x222222 );
+    this.material.shininess = 50;
+
+    // Sets the uniforms with the material values
+    this.material.uniforms.diffuse.value = new THREE.Color( 0x020202 );
+    this.material.uniforms.specular.value = this.material.specular;
+    this.material.uniforms.shininess.value = 10;//Math.max( this.material.shininess, 1e-4 );
+    this.material.uniforms.opacity.value =this. material.opacity;
+
+    this.randomShader = Math.random();
+    this.material.uniforms.random.value = this.randomShader;
+    this.material.lights = true;
+
+    //this.material = new THREE.ShaderMaterial( {
+    //    //uniforms:uniforms,
+    //    vertexShader: vertexShader2,
+    //    fragmentShader: fragmentShader
+    //} );
+
+    // create a sphere and assign the material
+    this.mesh = new THREE.Mesh(
+        new THREE.IcosahedronGeometry( 5, 4 ),
+        this.material
+    );
+
+    // magic here
+    this.mesh.customDepthMaterial = new THREE.ShaderMaterial({
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        uniforms: uniforms
+    });
+
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+
+    this.time = 0;
+    this.mesh.position.z = 70 + this.randomShader * -230;
+
+}
+
+Particle.prototype.getMesh = function(){
+    return this.mesh;
+};
+
+Particle.prototype.lookAt = function(aPosition, rotateX){
+    this.mesh.lookAt(aPosition);
+    this.mesh.rotateY(Math.PI);
+    this.mesh.rotateX(rotateX);
+    this.rotationInit = this.mesh.rotation.clone();
+};
+
+Particle.prototype.init = function(aOptions) {
+
+};
+
+Particle.prototype.update = function(aDelta, aAmplitudes) {
+
+    var zeroArray = [];
+    for(var i = 0; i < 256; i++)
+    {
+        zeroArray.push(aAmplitudes[i]);
+    }
+
+    this.time += aDelta;
+    this.mesh.customDepthMaterial.uniforms.time.value = this.time;
+    this.mesh.opacity = 0;
+    //this.material.uniforms.time.value = this.time;
+    this.material.uniforms.amplitudes.value = zeroArray;
+    this.mesh.customDepthMaterial.needsUpdate = true;
+};
---
+++ pages/visualAudioShader/js/ParticleManager.js
@@ -0,0 +1,134 @@
+/**
+ * Created by baptistebohelay on 17/01/17.
+ */
+"use strict";
+function ParticleManager(aScene)
+{
+    this.group = new THREE.Group();
+    this.particles = [];
+    for(var i = 0; i < 1; i++)
+    {
+        var lParticle = new Particle();
+        var mesh = lParticle.getMesh();
+        this.particles.push(lParticle);
+        this.group.add(mesh);
+    }
+
+    this.time = 0;
+    this.volume = 0.2;
+    this.counter = 0;
+    this.initSound();
+
+    //var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    //this.analyser = audioCtx.createAnalyser();
+    //this.analyser.fftSize = 2048;
+    this.sampleArrayFiltered = [];
+    this.dataArray = [];
+
+    var bufferLength = 128;
+    for(var i = 0; i < bufferLength; i++) {
+        this.dataArray.push(0);
+    }
+
+    aScene.add(this.group );
+}
+
+ParticleManager.prototype.initSound = function() {
+    var that  = this;
+
+    this.eventsSound = {
+
+        whileplaying: function() {
+            var nPeak = (this.peakData.left||this.peakData.right);
+            //// GIANT HACK: use EQ spectrum data for bass frequencies
+            //var eqSamples = 3;
+            //for (var i=0; i<eqSamples; i++)
+            //{
+            //    nPeak = (nPeak||this.eqData[i]);
+            //}
+            //that.dataArray = [];
+
+
+            if((Math.abs(parseFloat(this.waveformData.left[0])) + Math.abs(parseFloat(this.waveformData.left[10])) + Math.abs(parseFloat(this.waveformData.left[3]))) != 0)
+            {
+                that.counter++;
+                var lSamplesToMove = that.dataArray.length - 128;
+                that.dataArray = that.dataArray.slice(lSamplesToMove, that.dataArray.length);
+                if(that.counter % 3 == 0)
+                {
+                    that.dataArray.push(this.waveformData.right[0]);
+                }
+                else {
+                    var lastElValue = that.dataArray[that.dataArray.length - 1];
+                    var theOneBeforeValue = that.dataArray[that.dataArray.length - 2];
+                    that.dataArray[that.dataArray.length - 1] = (lastElValue * 0.3 + theOneBeforeValue * 0.7);
+                    that.dataArray.push(lastElValue);
+                }
+
+                //var samplesToSkip = 64;
+                //for(var i = 0; i < this.waveformData.left.length; i += samplesToSkip) {
+                //    that.dataArray.push(Math.abs(parseFloat(this.waveformData.left[i])));
+                //}
+            }
+            //sSoundAmplitude = (0.9+(nPeak*0.1));
+            //sLengthLegsSound.left = (sLengthLegsSound.left * 0.9 +(this.peakData.left*0.1));
+            //sLengthLegsSound.right = (sLengthLegsSound.right * 0.9 +(this.peakData.right*0.1));
+        },
+
+        stop: function()
+        {
+            sFoodArraySoundWait.push(sPlayingSound.particle);
+            sFoodArraySound.splice(0,1);
+            sMonsterSound.mParent.RemoveSound();
+        }
+    };
+
+    soundManager.onready(function() {
+        soundManager.defaultOptions.usePeakData = true;
+        soundManager.useFlashBlock = true;
+        // create sound
+        that.sound = soundManager.createSound({
+            id:'sound' + that.name,
+            url:"audio/aphex.mp3",
+            useWaveformData:true,
+            useEQData:false,
+            usePeakData:true,
+            volume:that.volume * 120,
+            whileplaying:that.eventsSound.whileplaying,
+            onstop:that.eventsSound.stop,
+            onfinish:that.eventsSound.stop
+        });
+    });
+};
+
+ParticleManager.prototype.update = function(aDelta) {
+    //
+    //this.analyser.getByteTimeDomainData(this.dataArray);
+
+    this.sampleArrayFiltered = [];
+    var samplesToPut = 512;
+    for(var i = 0; i < samplesToPut; i++) {
+        //this.sampleArrayFiltered.push((this.dataArray[i] - 128) / 128);
+        var lIndexInOriginArray = i;
+        this.sampleArrayFiltered.push(this.dataArray[i] * 15);
+    }
+
+    //this.group.rotation.y += aDelta * 0.2;
+    this.group.rotation.y = Math.PI * 0.5;
+    this.group.position.x = -50 ;//100  * Math.cos(this.time * 0.5);
+    this.time += aDelta;
+
+    for(var i = 0; i < this.particles.length; i++)
+    {
+        this.particles[i].update(aDelta, this.sampleArrayFiltered);
+    }
+};
+
+ParticleManager.prototype.togglePlay = function(aDelta) {
+    if(this.sound.playState == 0 || this.sound.paused) {
+        this.sound.play();
+    }
+    else {
+        this.sound.pause();
+    }
+};
